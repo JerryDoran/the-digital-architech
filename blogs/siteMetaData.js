1afd97076e83581c05ab70f27112608297b6a189
Jerome Doran <disneyfan9184@gmail.com>
2025-05-16 16:55:35 -0400

    Add logo image to the public images directory

---
+++ blogs/siteMetaData.js
@@ -0,0 +1,18 @@
+// please update required information
+const siteMetadata = {
+  title: 'Tech blog helping small businesses excel.',
+  author: 'The Web Architech',
+  headerTitle: 'Digital Architech',
+  description: 'Web Development & Software Solutions for Small Businesses',
+  language: 'en-us',
+  theme: 'system', // system, dark or light
+  siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
+  siteLogo: '/logo.png',
+  socialBanner: '/social-banner.png', // add social banner in the public folder
+  email: 'thewebarchitech@gmail.com',
+  github: 'https://github.com/JerryDoran',
+  facebook: 'https://facebook.com',
+  locale: 'en-US',
+};
+
+module.exports = siteMetadata;
